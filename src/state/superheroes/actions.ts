df9f71c69d2577420b1313bbeba8bb3577fab277
jordsami <sami.jord99@gmail.com>
2023-02-05 05:19:26 +0300

    fix action function type

--- src/state/superheroes/actions.ts
+++ src/state/superheroes/actions.ts
@@ -1,10 +1,13 @@
 import {THeroData} from '@HeroSpin/services';
+import {TSuperHeroesActionTypes} from './types';
 
 export const SET_HEROES = 'SET_HEROES';
 
-export const setSuperHeroesAction = (heroes: THeroData[]) => ({
+export const setSuperHeroesAction = (
+  heroes: THeroData[],
+): TSuperHeroesActionTypes => ({
   type: SET_HEROES,
   payload: {
-    heroes,
+    superheroes: heroes,
   },
 });
